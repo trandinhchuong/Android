d8aa200854850114a9e645f27498ab24a000c032
unknown <trandinhchuonght96@gmail.com>
2020-03-21 17:01:12 +0700

    add button

--- app/src/main/java/com/example/hbled/MainActivity.java
+++ app/src/main/java/com/example/hbled/MainActivity.java
@@ -3,18 +3,29 @@ package com.example.hbled;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button_home;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
       //  setContentView(R.layout.activity_main);
-
-
-
         setContentView(R.layout.blescan);
 
+        button_home =(Button)findViewById(R.id.button_home);
 
+        button_home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_main);
+
+            }
+        });
     }
+
 }
