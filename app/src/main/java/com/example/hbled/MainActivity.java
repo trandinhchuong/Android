fb23c3792d11fa689f1aaed5e8ad05f47783f6f1
unknown <trandinhchuonght96@gmail.com>
2020-03-21 15:52:56 +0700

    blescan.xml layout ok

--- app/src/main/java/com/example/hbled/MainActivity.java
+++ app/src/main/java/com/example/hbled/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.hbled;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+      //  setContentView(R.layout.activity_main);
+
+
+
+        setContentView(R.layout.blescan);
+
+
     }
 }
